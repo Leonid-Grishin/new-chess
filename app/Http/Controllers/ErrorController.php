9384d91adae6ec22244c787cc60730aa95d3aeb8
Leonid <alraun_work@mail.ru>
2025-12-14 22:13:49 +0300

    Обновил 404

--- app/Http/Controllers/ErrorController.php
+++ app/Http/Controllers/ErrorController.php
@@ -6,6 +6,11 @@ class ErrorController extends Controller
 {
     public function error404()
     {
-        return response()->view('errors.404', [], 404);
+        $meta = [
+            'title' => 'Такой страницы не существует',
+            'description' => 'Перейдите на главную страницу.',
+            'og-image' => 'images/camp/camp-og.jpg'
+        ];
+        return response()->view('errors.404', ['meta' => $meta,], 404);
     }
 }
